a765ad7d9ad0f1b34935a78cb3697fc32393eef5
niclundegard <nlundegard@protonmail.com>
2022-11-30 06:20:49 -0500

    fix foreign key to Category table

--- db/budget/Tables/Transactions.sql
+++ db/budget/Tables/Transactions.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Transactions]
   [id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
   [description] NVARCHAR(100) NOT NULL,
   [amount] MONEY,
+  [transaction_datetime] DATE NOT NULL,
   [category_id] INT NOT NULL
-  FOREIGN KEY (category_id) REFERENCES Transactions(id)
+  FOREIGN KEY (category_id) REFERENCES Category(id)
 )
